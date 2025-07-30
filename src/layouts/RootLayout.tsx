00458671cf9ec0794c7211284bc8cef0bbcf51db
Ahmadjonov Sobirjon <ahmadjonovsobirjon24@gmailcom>
2025-07-30 23:02:43 +0500

    first commit

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,19 @@
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import { Outlet } from "react-router-dom";
+
+const RootLayout = () => {
+  return (
+    <>
+      <div className="flex">
+        <Sidebar />
+        <div>
+          <Header />
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RootLayout;
