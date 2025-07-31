4d4714689b8e1bfe6f2a3af589630927cbf88339
Ahmadjonov Sobirjon <ahmadjonovsobirjon24@gmailcom>
2025-07-31 13:50:04 +0500

    update

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 const RootLayout = () => {
   return (
     <>
-      <div className="flex">
+      <div className="flex bg-[rgba(244,_249,_253,_1)]">
         <Sidebar />
         <div>
           <Header />
